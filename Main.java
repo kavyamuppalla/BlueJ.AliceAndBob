f45e412532d4c25abe38bbe65e4b48b5776e7e7c
Kavya Muppalla <kavyamuppalla@zipcoders-MBP-2.fios-router.home>
2019-05-22 22:24:41 -0400

    Finished Alice and Bob program

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args ){
-
+    public static void main(String[] args ) {
+        Scanner sr = new Scanner(System.in);
+        System.out.println("Enter Name:");
+        String name = sr.next();
+        if ((name.equals("Bob")) || (name.equals("Alice"))) {
+            System.out.println("Hello " +name);
+        }
+        
     }
+            
+        
 }
